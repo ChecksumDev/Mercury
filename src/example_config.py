6ccbd6d348a4c5ef617003761f2ded56329042f5
Checksum <24845326+ChecksumDev@users.noreply.github.com>
2021-12-19 23:23:15 -0800

    Create an example config.

---
+++ src/example_config.py
@@ -0,0 +1,15 @@
+# Configuration for Mercury
+
+# Domain: Must contain the protocol, leading and trailing slashes, and be a valid domain name.
+domain = ""  # "https://mercury.checksum.space/"
+
+# MongoDB settings
+mongo_settings = {
+    "host": "",
+    "user": "",
+    "password": "",
+    "db": "",
+}
+
+# Max file size in bytes
+max_file_size = 50 * 1024 * 1024  # 50 MB (default)
